850c1429b8e04360d305535a0da437af5192ddcf
Rselmane <rayan.selmane@etu.univ-smb.fr>
2025-03-14 16:39:51 +0100

    TD1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -27,28 +38,246 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var favorites = <WordPair>[];
+  var initialized = false;
+
+  String _wordPairsToJson(List<WordPair> pairs) {
+    return jsonEncode(pairs.map((pair) => {'first': pair.first, 'second': pair.second}).toList());
+  }
+
+  List<WordPair> _wordPairsFromJson(String json) {
+    List<dynamic> list = jsonDecode(json);
+    return list.map((item) => WordPair(item['first'], item['second'])).toList();
+  }
+
+  Future<bool> _saveToStorage() async {
+    var storage = await SharedPreferences.getInstance();
+    return await storage.setString('favorites', _wordPairsToJson(favorites));
+  }
+
+  Future<void> init() async {
+    if (initialized) {
+      return;
+    }
+
+    final storage = await SharedPreferences.getInstance();
+    final data = storage.getString('favorites');
+
+    if (data == null) {
+      favorites = <WordPair>[];
+    } else {
+      favorites = _wordPairsFromJson(data);
+    }
+
+    initialized = true;
+  }
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    _saveToStorage();
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    return Scaffold(
-  body: Column(
-    children: [
-      Text('Une idée aléatoire :'),
-      Text(appState.current.asLowerCase),
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('Aucun favori pour le moment.'),
+      );
+    }
 
-      // ↓ Ajoutez ceci
-      ElevatedButton(
-        onPressed: () {
-          print('Bouton appuyé !');
-        },
-        child: Text('Suivant'),
-      ),
-    ],
-  ),
-);
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('${appState.favorites.length} Favoris'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
 }
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10.0),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text("J'aime"),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Suivant'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  Future<void>? initializer;
+
+  @override
+  void initState() {
+    super.initState();
+    var appState = Provider.of<MyAppState>(context, listen: false);
+    initializer = appState.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('aucun composant pour $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 450) {
+        // Mobile layout
+        return Scaffold(
+          body: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: page,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Accueil',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'Favoris',
+              ),
+            ],
+            currentIndex: selectedIndex,
+            onTap: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+          ),
+        );
+      } else {
+        // Desktop layout
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Accueil'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favoris'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    });
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: pair.asPascalCase,
+        ),
+      ),
+    );
+  }
 }
